6e9c496110040f194472febdb018b47d49b1824a
Nathan <ncooke96@gmail.com>
2020-01-01 22:22:42 +0000

    Add unix tests

--- tests/testthat/test_LN.R
+++ tests/testthat/test_LN.R
@@ -8,41 +8,61 @@ test_that("MCMC_LN Converges",{
 
 
 test_that("LML_LN Returns Expected Result",{
-  if(.Machine$sizeof.pointer == 8 && .Platform$OS.type != "unix"){
+  if (.Machine$sizeof.pointer == 8){
+
 
     set.seed(123)
     LN <- MCMC_LN(N = 1000, thin = 20, burn = 40, Time = cancer[,1],
                   Cens = cancer[,2], X = cancer[,3:11])
     LN.LML <- LML_LN(thin = 20, Time = cancer[,1], Cens = cancer[,2],
                      X = cancer[,3:11], chain = LN)
-    testthat::expect_equal(round(as.numeric(LN.LML),2), c( -717.22, -0.27,
+
+    if (.Platform$OS.type != "unix"){
+      testthat::expect_equal(round(as.numeric(LN.LML),2), c( -717.22, -0.27,
                                                            0.75, 13.94,
                                                            -732.17))
+
+    } else{
+      testthat::expect_equal(round(as.numeric(LN.LML),2), c( -716.97, -0.07,
+                                                             0.78, 14.37,
+                                                             -732.19))
     }
+  }
 })
 
 test_that("DIC_LN Returns Expected Result",{
-  if(.Machine$sizeof.pointer == 8 && .Platform$OS.type != "unix"){
+  if(.Machine$sizeof.pointer == 8){
     set.seed(123)
 
     LN <- MCMC_LN(N = 1000, thin = 20, burn = 40, Time = cancer[,1],
                   Cens = cancer[,2], X = cancer[,3:11])
     LN.DIC <- DIC_LN(Time = cancer[,1], Cens = cancer[,2], X = cancer[,3:11],
                      chain = LN)
-    testthat::expect_equal(round(LN.DIC, 4), 1449.8554)
+
+    if( .Platform$OS.type != "unix"){
+      testthat::expect_equal(round(LN.DIC, 4), 1449.8554)
+    } else{
+      testthat::expect_equal(round(LN.DIC, 4), 1445.1216)
     }
+  }
 })
 
 test_that("CaseDeletion_LN Returns Expected Result",{
 
-  if(.Machine$sizeof.pointer == 8 && .Platform$OS.type != "unix"){
+  if(.Machine$sizeof.pointer == 8){
     set.seed(123)
     LN <- MCMC_LN(N = 1000, thin = 20, burn = 40, Time = cancer[,1],
                   Cens = cancer[,2], X = cancer[,3:11])
     LN.CD <- CaseDeletion_LN(Time = cancer[,1], Cens = cancer[,2],
                              X = cancer[,3:11], chain = LN)
     means <- round(c(mean(LN.CD[,1]), mean(LN.CD[,2]), mean(LN.CD[,3])), 4)
-    testthat::expect_equal(means, c(-5.312, 0.0487, 0.6068))}
+
+    if ( .Platform$OS.type != "unix"){
+      testthat::expect_equal(means, c(-5.312, 0.0487, 0.6068))
+      } else {
+      testthat::expect_equal(means, c(-5.2956, 0.0418, 0.6029))
+}
+  }
 })
 
 
