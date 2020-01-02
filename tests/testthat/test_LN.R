0231e957f7fbb1999f01be6e0fbe348e20f66110
Nathan <ncooke96@gmail.com>
2020-01-02 14:53:04 +0000

    Add additional tests

--- tests/testthat/test_LN.R
+++ tests/testthat/test_LN.R
@@ -1,3 +1,7 @@
+################################################################################
+################################ USER FUNCTIONS ################################
+################################################################################
+
 test_that("MCMC_LN Converges",{
   set.seed(123)
   LN.chain <- MCMC_LN(N = 1000, thin = 20, burn = 40, Time = c(5,10),
@@ -45,4 +49,67 @@ test_that("CaseDeletion_LN Returns Expected Result",{
     testthat::expect_equal(means, c(-5.312, 0.0487, 0.6068))}
 })
 
+################################################################################
+############################## INTERNAL FUNCTIONS ##############################
+################################################################################
 
+test_that("Prior_LN returns expect value for prior = 1",{
+
+  beta <- c(4,1,4)
+  sigma2 <- 0.1
+
+  p <- 1 + length(beta) / 2
+  aux <- -p * log(sigma2)
+  expect_equivalent(prior_LN(beta = c(4,1,4), sigma2 = 0.1, prior = 1,
+                             logs = T), aux )
+
+})
+
+test_that("Prior_LN returns expected value for prior = 2",{
+
+  beta <- c(4,1,4)
+  sigma2 <- 0.1
+  p <- 1
+  aux <- -p * log(sigma2)
+  expect_equivalent(prior_LN(beta = c(4,1,4), sigma2 = 0.1, prior = 2,
+                             logs = T), aux )
+
+})
+
+test_that("Prior_LN returns expected value for prior = 3 & logs = FALSE",{
+
+  beta <- c(4,1,4)
+  sigma2 <- 0.1
+  p <- 1
+  aux <- -p * sigma2
+  expect_equivalent(prior_LN(beta = c(4,1,4), sigma2 = 0.1, prior = 3,
+                             logs = F), aux )
+})
+
+test_that("MCMCR.sigma2.LN returns expected values for first row",{
+  set.seed(123)
+  chain <- MCMCR.sigma2.LN(N = 1000, thin = 20, Time = cancer[,1],
+                           Cens = cancer[,2], X = cancer[,3:11],
+                           beta0 =  seq(9), sigma20 = 1,
+                           logt0 = 1, prior = 2, set = 1)
+  expect_equal(chain[1,], c(seq(9), rep(1, 137) ))
+  expect_equal(round(chain[2,1],4), 1.5805)
+})
+
+test_that("log.lik.LN returns expected value for set = 0",{
+  set.seed(123)
+  lik <- log.lik.LN(Time = cancer[,1], Cens = cancer[,2], X = cancer[,3:11],
+                    beta = rep(0,9), sigma2 = 1, set = 0 , eps_l = 0.5,
+                    eps_r = 0.5)
+
+  expect_equal(round(lik,4), -1927.0135)
+})
+
+test_that("log.lik.LN returns expected values for set = 1",{
+  set.seed(123)
+  lik <- log.lik.LN(Time = cancer[,1], Cens = cancer[,2], X = cancer[,3:11],
+                    beta = rep(0,9), sigma2 = 1, set = 1 , eps_l = 0.5,
+                    eps_r = 0.5)
+
+  expect_equal(round(lik,4), -1926.7085)
+})
