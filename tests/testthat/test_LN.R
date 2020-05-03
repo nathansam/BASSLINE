e0b52464f799cb9d5c5b17b932de515fad7eacad
Nathan <ncooke96@gmail.com>
2020-05-03 14:41:03 +0100

    rewrite rtnorm (using msms:rtnorm as a basis)

--- tests/testthat/test_LN.R
+++ tests/testthat/test_LN.R
@@ -14,99 +14,6 @@ test_that("MCMC_LN returns expected number of rows when burn = 5,thin = 10", {
 })
 
 
-test_that("MCMC_LN same in C++ as in R", {
-  MCMC_LN_old <- function(N, thin, burn, Time, Cens, X, beta0 = NULL,
-                          sigma20 = NULL, prior = 2, set = 1, eps_l = 0.5,
-                          eps_r = 0.5) {
-
-    # Sample starting values if not given
-    if (is.null(beta0)) beta0 <- beta.sample(n = ncol(X))
-    if (is.null(sigma20)) sigma20 <- sigma2.sample()
-
-    MCMC.param.check(N, thin, burn, Time, Cens, X, beta0, sigma20,
-                     prior, set, eps_l, eps_r)
-
-    k <- length(beta0)
-    n <- length(Time)
-    N.aux <- round(N / thin, 0)
-    if (prior == 1) {
-        p <- 1 + k / 2
-    }
-    if (prior == 2) {
-        p <- 1
-    }
-
-    beta <- matrix(rep(0, times = (N.aux + 1) * k), ncol = k)
-    beta[1, ] <- beta0
-    sigma2 <- rep(0, times = N.aux + 1)
-    sigma2[1] <- sigma20
-    logt <- matrix(rep(0, times = (N.aux + 1) * n), ncol = n)
-    logt[1, ] <- log(Time)
-
-    beta.aux <- beta[1, ]
-    sigma2.aux <- sigma2[1]
-    logt.aux <- logt[1, ]
-
-    for (iter in 2:(N + 1)) {
-        mu.aux <- solve(t(X) %*% X) %*% t(X) %*% logt.aux
-        Sigma.aux <- sigma2.aux * solve(t(X) %*% X)
-        beta.aux <- mvrnormArma(n = 1, mu = mu.aux, Sigma = Sigma.aux)
-
-
-        shape.aux <- (n + 2 * p - 2) / 2
-        rate.aux <- 0.5 * t(logt.aux - X %*% beta.aux) %*% (logt.aux - X %*%
-                                                                beta.aux)
-        if (rate.aux > 0 & is.na(rate.aux) == FALSE) {
-            sigma2.aux <- (stats::rgamma(1, shape = shape.aux,
-                                         rate = rate.aux)) ^ (-1)
-        }
-
-
-        logt.aux <- logt_update_SMLN(Time, Cens, X, beta.aux, sigma2.aux, set,
-                                     eps_l, eps_r)
-
-        if (iter %% thin == 0) {
-            beta[iter / thin + 1, ] <- beta.aux
-            sigma2[iter / thin + 1] <- sigma2.aux
-            logt[iter / thin + 1, ] <- logt.aux
-        }
-        if ((iter - 1) %% 1e+05 == 0) {
-            cat(paste("Iteration :", iter, "\n"))
-        }
-    }
-
-    chain <- cbind(beta, sigma2, logt)
-
-
-
-    beta.cols <- paste("beta.", seq(ncol(X)), sep = "")
-    logt.cols <- paste("logt.", seq(length(Time)), sep = "")
-    colnames(chain) <- c(beta.cols, "sigma2", logt.cols)
-
-    if (burn > 0) {
-      burn.period <- 1:(burn / thin)
-      chain <- chain [-burn.period, ]
-    }
-
-    return(chain)
-  }
-
-#
-#   set.seed(123)
-#   Result.Cpp <- MCMC_LN(N = 1000, thin = 20, burn = 40,
-#                         Time = cancer[, 1], Cens = cancer[, 2],
-#                         X = cancer[, 3:11])
-#
-#   set.seed(123)
-#   Result.R <- MCMC_LN_old(N = 1000, thin = 20, burn = 40,
-#                                         Time = cancer[, 1], Cens = cancer[, 2],
-#                                         X = cancer[, 3:11])
-#
-#   testthat::expect_equal(Result.Cpp, Result.R)
-
-
-
-})
 
 
 ################################################################################
@@ -189,7 +96,7 @@ test_that("logt_update_SMLN same in C++ as in R",{
                                  eps_r)
 
   expect_equal(as.numeric(result.R), as.numeric(result.Cpp))
-  
+
 })
 
 
--- tests/testthat/test_rtnorm.R
+++ tests/testthat/test_rtnorm.R
@@ -1,71 +1,51 @@
-test_that("rtnorm same as rtruncnorm when all arguments are vectors", {
+test_that("rtnorm same as msm version for finite limits", {
   set.seed(123)
-  Cpp.result <- rtnorm(n = 2, lower = c(-1, -1), upper = c(1, 1),
-                       mu = c(0, 0), sd = c(1, 1))
-  Cpp.result <- as.vector(Cpp.result)
-  
-  set.seed(123)
-  R.result <- truncnorm::rtruncnorm(n = 2, a = c(-1, -1),
-                                    b = c(1, 1), mean = c(0, 0),
-                                    sd = c(1, 1))
-  expect_equal(Cpp.result, R.result)
-})
-
-test_that("rtnorm same as rtruncnorm when none of the arguments are vectors", {
-  set.seed(123)
-  Cpp.result <- rtnorm(n = 5, lower = -1, upper = Inf,
+  Cpp.result <- rtnorm(n = 1, lower = -1, upper =  1,
                        mu = 0, sd = 1)
   Cpp.result <- as.vector(Cpp.result)
-  
+
   set.seed(123)
-  R.result <- truncnorm::rtruncnorm(n = 5, a = -1,
-                                    b = Inf, mean = 0,
+  R.result <- msm::rtnorm(n = 1, lower = -1,
+                                    upper = 1, mean = 0,
                                     sd = 1)
   expect_equal(Cpp.result, R.result)
 })
 
-test_that("rtnorm same as rtruncnorm when SD isn't a vector", {
 
-  #  Test when SD isn't a vector
+test_that("rtnorm same as msm version for inf upper", {
   set.seed(123)
-  Cpp.result <- rtnorm(n = 2, lower = c(-1, -1), upper = c(Inf, Inf),
-                       mu = c(0, 0), sd = 1)
+  Cpp.result <- rtnorm(n = 1, lower = -1, upper =  Inf,
+                       mu = 0, sd = 1)
   Cpp.result <- as.vector(Cpp.result)
-  
+
   set.seed(123)
-  R.result <- truncnorm::rtruncnorm(n = 2, a = c(-1, -1),
-                                    b = c(Inf, Inf), mean = c(0, 0), sd = 1)
+  R.result <- msm::rtnorm(n = 1, lower = -1,
+                          upper = Inf, mean = 0,
+                          sd = 1)
   expect_equal(Cpp.result, R.result)
 })
 
-test_that("rtnorm same as rtruncnorm when lower/a isn't a vector", {
-  # Test when lower / a isn't a vector
+test_that("rtnorm same as msm version for inf lower", {
   set.seed(123)
-  Cpp.result <- rtnorm(n = 2, lower = -1, upper = c(1, 1), mu = c(0, 0),
-                       sd = c(1, 1))
+  Cpp.result <- rtnorm(n = 1, lower = -Inf, upper =  1,
+                       mu = 0, sd = 1)
   Cpp.result <- as.vector(Cpp.result)
-  
+
   set.seed(123)
-  R.result <- truncnorm::rtruncnorm(n = 2, a = -1,
-                                    b = c(1, 1), mean = c(0, 0),
-                                    sd = c(1, 1))
-  expect_equal(Cpp.result, R.result)
-})
-  
-test_that("rtnorm same as rtruncnorm when upper/b isn't a vector", {
-  # Test when upper / b isn't a vector
-  set.seed(123)
-  Cpp.result <- rtnorm(n = 2, lower = c(-1, -1), upper = Inf, mu = c(0, 0),
-                       sd = c(1, 1))
-  Cpp.result <- as.vector(Cpp.result)
-  set.seed(123)
-  R.result <- truncnorm::rtruncnorm(n = 2, a = c(-1, -1),
-                                    b = Inf, mean = c(0, 0),
-                                    sd = c(1, 1))
-  
+  R.result <- msm::rtnorm(n = 1, lower = -Inf,
+                          upper = 1, mean = 0,
+                          sd = 1)
   expect_equal(Cpp.result, R.result)
 })
 
-test_that("rtnorm returns same result when called from R as from C++", {
-  R.result <- rtnorm(n = 3, lower = -Inf, upper = 1, mu = 0, sd = 1)
+test_that("rtnorm with inf limits same as rnorm", {
+  set.seed(123)
+  Cpp.result <- rtnorm(n = 1, lower = -Inf, upper =  Inf,
+                       mu = 0, sd = 1)
+  Cpp.result <- as.vector(Cpp.result)
+
+  set.seed(123)
+  R.result <- rnorm(1, 0, 1)
+  expect_equal(Cpp.result, R.result)
 })
+
